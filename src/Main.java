fc3fc3724cd312bd55797336c74028f5e3de7651
Amir B. Kahna <amir.kahna2@gmail.com>
2026-06-04 11:47:36 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import exercise3.Ex3;
 import exercise4.Ex4;
 
 public class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println(Ex1.leapYear(2100));
         System.out.println(Ex1.oddOrEven(10));
         System.out.println(Ex2.numberSpelling());
